4b1c197675bbf8d4dc50492e23995ca99fbeb9b5
MegCheppy <88152028+MegCheppy@users.noreply.github.com>
2022-08-11 01:40:38 +0300

    Merge pull request #3 from MegCheppy/bugfix Merge pull request #2 from MegCheppy/main on unit tests

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -52,46 +52,54 @@ class TestTweetDfExtractor(unittest.TestCase):
         # tweet_df = self.df.get_tweet_df()
 
     def test_find_statuses_count(self):
-        self.assertEqual(
-            self.df.find_statuses_count(), <provide a list of the first five status counts>
+        self.assertEqual (
+            self.df.find_statuses_count(), [8097,5831,1627,1627,18958] #<provide a list of the first five status counts>
         )
 
     def test_find_full_text(self):
-        text = <provide a list of the first five full texts>
+        text = [
+            "RT @i_ameztoy: Extra random image (I):\n\nLets focus in one very specific zone of the western coast -&gt; Longjing District, Taichung #City, #Ta…",
+            "RT @IndoPac_Info: #China's media explains the military reasons for each area of the drills in the #Taiwan Strait\n\nRead the labels in the pi…",
+            "China even cut off communication, they don't anwer phonecalls from the US. But here clown @ZelenskyyUa enters the stage to ask #XiJinping to change Putin's mind.",
+            "Putin to #XiJinping : I told you my friend, Taiwan will be a vassal state, including nukes, much like the Ukrainian model. I warned you... But it took Pelosi to open China's eyes.",
+            "RT @ChinaUncensored: I\’m sorry, I thought Taiwan was an independent country because it had its own government, currency, military, travel d…"
+            ] #<provide a list of the first five full texts>
 
         self.assertEqual(self.df.find_full_text(), text)
 
     def test_find_sentiments(self):
         self.assertEqual(
             self.df.find_sentiments(self.df.find_full_text()),
-            (
-                <provide a list of the first five sentiment values>,
-                <provide a list of the first five polarity values>,
+            ([-0.125, -0.1, 0.0, 0.1, -6.938893903907228e-18],
+              [0.190625,0.1,0.0,0.35,0.55625],
+               # <provide a list of the first five sentiment values>,
+                #<provide a list of the first five polarity values>,
             ),
         )
 
 
     def test_find_screen_name(self):
-        name = <provide a list of the first five screen names>
+        name = ['i_ameztoy','ZIisq','Fin21Free','Fin21Free','VizziniDolores'] #<provide a list of the first five screen names>
         self.assertEqual(self.df.find_screen_name(), name)
 
     def test_find_followers_count(self):
-        f_count = <provide a list of the first five follower counts>
+        f_count =[20497,65,85,85,910] #<provide a list of the first five follower counts>
         self.assertEqual(self.df.find_followers_count(), f_count)
 
     def test_find_friends_count(self):
-        friends_count = <provide a list of the first five friend's counts>
+        friends_count =[2621,272,392,392,2608] #<provide a list of the first five friend's counts>
         self.assertEqual(self.df.find_friends_count(), friends_count)
 
     def test_find_is_sensitive(self):
-        self.assertEqual(self.df.is_sensitive(), <provide a list of the first five is_sensitive values>)
+        self.assertEqual(self.df.is_sensitive(),[
+                         None, None, None, None, None]) #<provide a list of the first five is_sensitive values>)
 
 
-    # def test_find_hashtags(self):
-    #     self.assertEqual(self.df.find_hashtags(), )
+    def test_find_hashtags(self):
+         self.assertEqual(self.df.find_hashtags(), )
 
-    # def test_find_mentions(self):
-    #     self.assertEqual(self.df.find_mentions(), )
+    def test_find_mentions(self):
+         self.assertEqual(self.df.find_mentions(), )
 
 
 
